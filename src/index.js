0d9ca6a2b2f3be5abfada019e5bcfa8b9b9858b4
Alejandra Izaguirre <aleizatebar@gmail.com>
2020-03-29 11:35:01 +0200

    JSX vs HTML

--- src/index.js
+++ src/index.js
@@ -3,8 +3,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create a react component
+
+/*
+ > class="" changes to className=""
+ > we add style with double {{}} and removing '-' on properties names
+ > JSX can reference JS variables with interpolation {nameOfVariable}
+*/
+
+function getButtonText () {
+    return 'Click on me!';
+}
+
 const App = () => {
-    return <div>Hi there!</div>;
+    const buttonText = 'Click Me!'
+
+    return (
+    <div>
+        <label className="label" for="name">Enter name: </label>
+        <input id="name" type="text"/>
+        <button style={{backgroundColor: 'blue', color: 'white'}}>
+            {buttonText} {getButtonText()}
+        </button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
